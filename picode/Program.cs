b0742cf38315c3daeb451a6fb5eb23d14d585e15
Alex Stone <22AStone@heckgrammar.co.uk>
2026-04-27 14:14:31 +0100

    ...

--- picode/Program.cs
+++ picode/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace picode
+﻿using System.Device.Gpio;
+
+namespace picode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            GpioController controller = new(PinNumberingScheme.Board);
+            controller.OpenPin(40, PinMode.Output);
+            controller.Write(40, PinValue.High);
+            controller.ClosePin(40);
+
         }
     }
 }
