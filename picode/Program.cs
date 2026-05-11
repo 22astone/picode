8e1a8cbe1d3131e6951b51049fad14c59b849d8b
Alex Stone <22AStone@heckgrammar.co.uk>
2026-05-11 14:13:21 +0100

    ,

--- picode/Program.cs
+++ picode/Program.cs
@@ -1,17 +1,40 @@
 ﻿using System.Device.Gpio;
 
-namespace picode
+namespace LEDPattern
 {
     internal class Program
     {
+        static GpioController controller = new(PinNumberingScheme.Board);
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            GpioController controller = new(PinNumberingScheme.Board);
-            controller.OpenPin(40, PinMode.Output);
-            controller.Write(40, PinValue.High);
-            controller.ClosePin(40);
+            int red = 16;
+            int yellow = 18;
+            int green = 22;
+            int blue = 36;
+
+
+            controller.OpenPin(red, PinMode.Output);
+            int times = 40;
+            while (true)
+            {
+                Flash(red, times);
+                Flash(yellow, times);
+                Flash(green, times);
+                Flash(blue, times);
+            }
+
+            static void Flash(int pin, int time)
+            {
+                controller.OpenPin(pin, PinMode.Output);
+                controller.Write(pin, PinValue.High);
+                Thread.Sleep(time);
+                controller.Write(pin, PinValue.Low);
+                controller.ClosePin(pin);
+            }
 
         }
     }
 }
+
+
